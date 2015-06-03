7def479fd7451531b5e3d11b18f2e493b709fc80
Jenan Mannette <jenanmannette@gmail.com>
2015-06-03 11:18:41 -0600

    simple module exercise

---
+++ app.js
@@ -0,0 +1,7 @@
+var foo = require("./lib.js");
+
+foo.add(1,2);
+foo.sub(5,3);
+foo.multi(9,2);
+foo.divi(10,50);
+foo.removeBadPeople(people)
---
+++ lib.js
@@ -0,0 +1,17 @@
+module.exports = {
+    add: function(num1, num2) {
+        console.log(num1 + num2);
+    },
+    sub: function(num1, num2) {
+        console.log(num1 - num2);
+    },
+    multi: function(num1, num2) {
+        console.log(num1 * num2);
+    },
+    divi: function(num1, num2) {
+        console.log(num1 / num2);
+    },
+    removeBadPeople: function() {
+        //function
+    }
+}
